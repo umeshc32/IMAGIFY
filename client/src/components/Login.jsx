34c3d0c7a73b4069af060ee966500b4e8649c2f4
Umesh Cirvi <138509923+umeshc32@users.noreply.github.com>
2025-07-11 20:14:54 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -86,7 +86,7 @@ const Login = () => {
           <input onChange={e => setPassword(e.target.value)} value={password} type="password" className='outline-none text-sm' placeholder='Password' required />
         </div>
 
-        <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forget password?</p>
+      
 
         <button className='bg-blue-600 w-full text-white py-2 rounded-full'>{state == 'Login' ? 'login' : 'Create Account'}</button>
 
